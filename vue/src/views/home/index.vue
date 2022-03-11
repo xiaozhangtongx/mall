8e41971ef614592824cd39c14988311eba062fe2
xiaozhangtongx <2446159171@qq.com>
2022-03-11 23:03:33 +0800

    add img

--- vue/src/views/home/index.vue
+++ vue/src/views/home/index.vue
@@ -2,8 +2,8 @@
   <div>
     <!-- 轮播图 -->
     <el-carousel :interval="4000" indicator-position="outside" height="400px">
-      <el-carousel-item v-for="item in 6" :key="item">
-        <h3 class="medium">图片{{ item }}</h3>
+      <el-carousel-item v-for="item in imgs" :key="item">
+        <h3 class="medium"><img :src="item" alt="" /></h3>
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -13,26 +13,42 @@
   export default {
     name: 'home',
     data() {
-      return {}
+      return {
+        imgs: [],
+      }
     },
-    methods: {},
+    methods: {
+      // 获取images文件夹下面的图片
+      getImgs() {
+        let res = require.context('./images/', false, /.jpg$/).keys()
+        this.imgs = res.map((item) => {
+          let img = './images/' + item.replace('./', '')
+          console.log(img)
+          return require(img + '')
+        })
+      },
+    },
+    created() {
+      this.getImgs()
+    },
   }
 </script>
 
 <style scoped lang="less">
   .el-carousel__item h3 {
-    color: #475669;
     font-size: 14px;
     opacity: 0.75;
     line-height: 200px;
     margin: 0;
   }
-
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-
-  .el-carousel__item:nth-child(2n + 1) {
-    background-color: #d3dce6;
+  .medium {
+    > img {
+      width: 100%;
+      cursor: pointer;
+      transition: all 0.6s;
+      &:hover {
+        transform: scale(1.4);
+      }
+    }
   }
 </style>
